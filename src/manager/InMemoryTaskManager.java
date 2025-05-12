f4a1389565b2d5e581148ad44e2e212ae1d301af
Artem Pchelintsev <pchelintsev.art@gmail.com>
2025-05-12 16:54:29 +0300

    убран неиспользованный импорт

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -9,7 +9,6 @@ import tasks.Task;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class InMemoryTaskManager implements TaskManager {
     int incrementId = 1;
